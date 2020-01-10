dad7b1ad6ed5412e459dcf321593a506606eb661
ally_yousefi_geeksltd <ally.yousefi@geeks.ltd.uk>
2020-01-10 14:18:29 +0000

    Intial comment

---
+++ WorldRemit/Controllers/ProductsListController.cs
@@ -0,0 +1,62 @@
+﻿using WorldRemit.Model;
+using WorldRemit.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldRemit.Controllers
+{
+    public class ProductsListController : Controller
+    {
+        [Route("string")]
+        public string String()
+        {
+            return "This is a string from a controller";
+        }
+        
+        public IActionResult Index()
+        {
+          
+            return View("~/Views/ProductsList.cshtml", Database.Products);
+        }
+        public IActionResult Add()
+        {
+            return View("~/Views/ProductEntre.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            Database.Products.Add(product);
+            return RedirectToAction("Index");
+
+        }
+
+        
+        public IActionResult Edit(int id)
+        {
+            var product = Database.Products.Single(p => p.ID == id);
+            return View("~/Views/ProductEntre.cshtml", product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            var result = Database.Products.Single(p => p.ID == product.ID);
+            result.Name = product.Name;
+            result.Company = product.Company;
+            return RedirectToAction("Index");            
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = Database.Products.Single(p => p.ID == id);
+            Database.Products.Remove(product);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WorldRemit/Services/Database.cs
@@ -0,0 +1,35 @@
+﻿using WorldRemit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldRemit.Services
+{
+    public class Database
+    {
+        public static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Name = "Iphone",
+                ID = 1,
+                Company = "Apple"
+            },
+            new Product
+            {
+                Name = "Android",
+                ID = 2,
+                Company = "Samsung"
+            },
+            new Product
+            {
+                Name = "Windows",
+                ID = 3,
+                Company = "Microsoft"
+            }
+        };
+
+
+    }
+}
